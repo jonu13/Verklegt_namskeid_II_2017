c018f2d860a6abf5f73668ccbb8c744f929e9b2f
Tryggvi Bragason <tryggvi4@gmail.com>
2017-05-08 15:56:52 +0000

    SignalR, notendur sjá breytingar hjá hvor öðrum

---
+++ WebEditor/Hubs/CodeHub.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using WebEditor.Services;
+
+namespace WebEditor.Hubs
+{
+    public class CodeHub : Hub
+    {
+        private ProjectService _service = new ProjectService();
+        public void JoinDocument(int documentID)
+        {
+            Groups.Add(Context.ConnectionId, Convert.ToString(documentID));
+        }
+
+        public void OnChange(object changeData, int documentID)
+        {
+            Clients.Group(Convert.ToString(documentID), Context.ConnectionId).OnChange(changeData);
+        }
+    }
+}
